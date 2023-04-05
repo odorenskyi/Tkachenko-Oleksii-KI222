eaf702540e341f0ea8e295676ecf316d7bf5dfc8
Sans91 <sansomailjokes@gmail.com>
2023-04-05 19:54:45 +0300

    Вніс зміни до бібліотеки та до заголовкового фалу, додав текстові файли main.txt

--- lab08/prj/ModulesTkachenkoOY.h
+++ lab08/prj/ModulesTkachenkoOY.h
@@ -1,10 +1,13 @@
 #ifndef MODULESTKACHENKOOY_H_INCLUDED
 #define MODULESTKACHENKOOY_H_INCLUDED
-struct result{
-    float results[3];
+
+struct results{
+    float totalWage, processedWage, totalTax;
+    std::string sockSizeEU;
+    int sockSizeUS, numberOfBits, typeOfBits;
 };
 float s_calculation(float x, float y, float z);
-result wagePaid(int a = 0, int b = 0, int c = 0, int d = 0 , int e = 0);
-result maleSocksSize(int sockSize = 0 );
-int bitInNumber(int num = -1);
+results wagePaid(int a = 0, int b = 0, int c = 0, int d = 0 , int e = 0);
+results maleSocksSizeConvert(int sockSize = 0 );
+results bitInNumber(int num = -1);
 #endif // MODULESTKACHENKOOY_H_INCLUDED
